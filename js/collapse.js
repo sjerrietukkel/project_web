e04f4949d223040823a2ba9974f5ff6bce5a23ad
Daniel van der Schuur <danielvdschuur@gmail.com>
2018-08-26 23:29:53 +0200

    Tags added

---
+++ js/collapse.js
@@ -0,0 +1,24 @@
+const accordion = document.getElementById("accordion");
+const content = document.getElementById("content");
+
+accordion.addEventListener('click', (event) => {
+    if (content.style.display === "none") {
+        content.style.display = "flex"
+    } else {
+        content.style.display = "none";
+    }
+})
+
+const accordion2 = document.getElementById("accordion2");
+const content2 = document.getElementById("content2");
+
+accordion2.addEventListener('click', (event) => {
+    if (content2.style.display === "none") {
+        content2.style.display = "flex"
+    } else {
+        content2.style.display = "none";
+    }
+})
+
+
+console.log("doet ie het?")
---
+++ js/tags_active.js
@@ -0,0 +1,65 @@
+var tags = document.querySelector('.tags');
+tags.onclick = function() {
+    tags.classList.toggle('liactive');
+    console.log('clicked!');
+  }
+
+var tags2 = document.querySelector('.tags2');
+tags2.onclick = function() {
+    tags2.classList.toggle('liactive');
+    console.log('clicked!');
+}  
+
+var tags3 = document.querySelector('.tags3');
+tags3.onclick = function() {
+    tags3.classList.toggle('liactive');
+    console.log('clicked!');
+}    
+
+var tags4 = document.querySelector('.tags4');
+tags4.onclick = function() {
+    tags4.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+  var tags5 = document.querySelector('.tags5');
+tags5.onclick = function() {
+    tags5.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags6 = document.querySelector('.tags6');
+tags6.onclick = function() {
+    tags6.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags7 = document.querySelector('.tags7');
+tags7.onclick = function() {
+    tags7.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags8 = document.querySelector('.tags8');
+tags8.onclick = function() {
+    tags8.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags9 = document.querySelector('.tags9');
+tags9.onclick = function() {
+    tags9.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags10 = document.querySelector('.tags10');
+tags10.onclick = function() {
+    tags10.classList.toggle('liactive');
+    console.log('clicked!');
+}
+
+var tags11 = document.querySelector('.tags11');
+tags11.onclick = function() {
+     tags11.classList.toggle('liactive');
+    console.log('clicked!');
+}
